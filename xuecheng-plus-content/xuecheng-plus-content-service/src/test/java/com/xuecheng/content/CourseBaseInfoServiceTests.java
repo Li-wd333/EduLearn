d5a5a209bb67f5dab13d0793dc36600450dfb487
Li-wd333 <2944413731@qq.com>
2025-05-13 16:33:34 +0800

    网关搭建和nacos配置

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseInfoServiceTests.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseInfoServiceTests.java
@@ -14,7 +14,7 @@ import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 public class CourseBaseInfoServiceTests {
     @Autowired
     private CourseBaseInfoService courseBaseInfoService;
